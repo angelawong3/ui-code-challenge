8593308e8feff9dc63ca4eff5d115aebf872a4f4
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-09-14 13:30:24 +0100

    create files

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => (
+  <div className="side-bar">
+    <nav>
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Navigation;
